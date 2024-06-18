1741e862c251ebf099c6e4acb9e16cd8cc4314c9
miloshadzic86 <zvrkatwork@gmail.com>
2024-06-18 20:27:43 +0200

    conform works properly now

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,20 +1,20 @@
 -- templ specific commands
-local templ_format = function()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local filename = vim.api.nvim_buf_get_name(bufnr)
-  local cmd = "templ fmt " .. vim.fn.shellescape(filename)
-
-  vim.fn.jobstart(cmd, {
-    on_exit = function()
-      -- Reload the buffer only if it's still the current buffer
-      if vim.api.nvim_get_current_buf() == bufnr then
-        vim.cmd("e!")
-      end
-    end,
-  })
-end
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = templ_format })
+-- local templ_format = function()
+--   local bufnr = vim.api.nvim_get_current_buf()
+--   local filename = vim.api.nvim_buf_get_name(bufnr)
+--   local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+--
+--   vim.fn.jobstart(cmd, {
+--     on_exit = function()
+--       -- Reload the buffer only if it's still the current buffer
+--       if vim.api.nvim_get_current_buf() == bufnr then
+--         vim.cmd("e!")
+--       end
+--     end,
+--   })
+-- end
+--
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = templ_format })
 vim.filetype.add({ extension = { templ = "templ" } })
 
 return {
