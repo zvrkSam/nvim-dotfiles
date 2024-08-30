3c511659478345b15f5bba07b1fcfd77bf0bb1ce
miloshadzic86 <zvrkatwork@gmail.com>
2024-08-30 12:59:23 +0200

    Updated telescope to flex view

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,13 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     defaults = {
+      layout_strategy = "flex",
+      layout_config = {
+        flip_columns = 150,
+        vertical = {
+          preview_height = 0.6,
+        },
+      },
       file_ignore_patterns = {
         "node_modules",
       },
