e36846061781366d97923f1725a398b45e4577e3
miloshadzic86 <zvrkatwork@gmail.com>
2024-09-02 09:52:39 +0200

    Icons are overriden now

---
+++ lua/plugins/devicons.lua
@@ -0,0 +1,13 @@
+-- Color reference
+-- https://github.com/echasnovski/mini.icons/blob/12e7b5d47bfc1b4c5ba4278fb49ec9100138df14/doc/mini-icons.txt#L150-L165
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    extension = {
+      go = { glyph = "", hl = "MiniIconsAzure" },
+      templ = { glyph = "", hl = "MiniIconsYellow" },
+      sh = { hl = "MiniIconsGreen" },
+      mdx = { glyph = "" },
+    },
+  },
+}
