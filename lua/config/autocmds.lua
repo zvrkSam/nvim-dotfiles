4caf8860f0f8955da4c038402efe8cb87e8083db
miloshadzic86 <zvrkatwork@gmail.com>
2024-09-13 21:36:30 +0200

    Added search by directory

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,18 +9,30 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-local function create_telescope_command(command_name, file_pattern, prompt_title)
+local telescope = require("telescope.builtin")
+
+local function search_by_file_type(command_name, file_pattern, prompt_title)
   vim.api.nvim_create_user_command(command_name, function()
-    require("telescope.builtin").find_files({
+    telescope.find_files({
       find_command = { "rg", "--files", "--glob", file_pattern },
       prompt_title = prompt_title,
     })
   end, {})
 end
 
-create_telescope_command("TelescopeGo", "*.go", "Find golang Files")
-create_telescope_command("TelescopeMD", "*.md", "Find markdown Files")
-create_telescope_command("TelescopeMDX", "*.mdx", "Find mdx Files")
-create_telescope_command("TelescopeTSX", "*.tsx", "Find tsx Files")
-create_telescope_command("TelescopeTS", "*.ts", "Find ts Files")
-create_telescope_command("TelescopeAstro", "*.astro", "Find astro Files")
+local function search_by_directory(command_name, dir, prompt_title)
+  vim.api.nvim_create_user_command(command_name, function()
+    telescope.find_files({
+      cwd = vim.fn.expand(dir),
+      prompt_title = prompt_title,
+    })
+  end, {})
+end
+
+search_by_file_type("TelescopeGo", "*.go", "Find golang Files")
+search_by_file_type("TelescopeMD", "*.md", "Find markdown Files")
+search_by_file_type("TelescopeMDX", "*.mdx", "Find mdx Files")
+search_by_file_type("TelescopeTSX", "*.tsx", "Find tsx Files")
+search_by_file_type("TelescopeTS", "*.ts", "Find ts Files")
+search_by_file_type("TelescopeAstro", "*.astro", "Find astro Files")
+search_by_directory("SearchNotes", "/home/zvrksam/dev/notes", "Search markdown notes")
