21da559b8a267099e7fabdb11d9c24e41c309aa6
miloshadzic86 <zvrkatwork@gmail.com>
2024-09-14 10:04:36 +0200

    Got tab working again for cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,8 +1,11 @@
-local cmp = require("cmp")
-cmp.setup({
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-})
-return {}
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    }
+    return opts
+  end,
+}
