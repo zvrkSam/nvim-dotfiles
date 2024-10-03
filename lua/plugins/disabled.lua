448cf418c930cf6a5e14b24bf20f9b516939f054
miloshadzic86 <zvrkatwork@gmail.com>
2024-10-03 04:24:41 +0200

    Disabled indent-blankline.nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "ThePrimeagen/harpoon", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 
   -- these two are realated to Markdown
 }
