ee76a5a054bae382dc2c57ae2a3548dc3d507245
miloshadzic86 <zvrkatwork@gmail.com>
2024-10-03 03:23:30 +0200

    Dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,35 @@
+return {
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  opts = function(_, opts)
+    local logo = [[ 
+                                       
+       ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          
+        ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       
+              ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     
+               ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    
+              ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   
+       ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  
+      ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   
+     ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  
+     ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ 
+          ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     
+           ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     
+    ]]
+    logo = string.rep("\n", 15) .. logo .. "\n\n"
+    opts.config.header = vim.split(logo, "\n")
+    opts.theme = "doom"
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyVimStarted",
+      callback = function()
+        local colors = require("tokyonight.colors").setup()
+        vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#7951A0" })
+        vim.api.nvim_set_hl(0, "DashboardFooter", { fg = colors.fg_dark })
+        vim.api.nvim_set_hl(0, "DashboardDesc", { fg = colors.purple })
+        vim.api.nvim_set_hl(0, "DashboardIcon", { fg = "#c2a4f8" })
+        vim.api.nvim_set_hl(0, "DashboardKey", { fg = "#FD7132" })
+      end,
+    })
+  end,
+}
