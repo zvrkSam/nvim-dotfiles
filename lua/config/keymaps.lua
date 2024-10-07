b2f363c70ce1a2d6a170d7e15300c64a43fd8a42
miloshadzic86 <zvrkatwork@gmail.com>
2024-10-07 12:26:56 +0200

    Formating update and added delete functionality to ctrl+x

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,35 +4,57 @@
 
 local map = vim.keymap.set
 
+-------------------------
+----- WINDOW/BUFFER -----
+-------------------------
+
 -- Delete buffer
-map("n", "<leader>d", ":bdelete<cr>", { desc = "Delete buffer", noremap = true, silent = true })
+map("n", "<leader>d", ":bdelete<CR>", { desc = "Delete buffer", noremap = true, silent = true })
 
 -- Delete window
 map("n", "<leader>W", ":close<CR>", { desc = "Delete window", noremap = true, silent = true })
 
--- Telescope pull-up notes
-map("n", "<leader>fn", ":SearchNotes<cr>", { desc = "Lookup notes", noremap = true, silent = true })
+-- Resizing of windows
+map("n", "<A-.>", "<c-w>10<")
+map("n", "<A-,>", "<c-w>10>")
+map("n", "<A-t>", "<c-W>+10")
+map("n", "<A-s>", "<c-W>-10")
 
--- Telescope lookup specific file type
-map("n", "<leader>fG", ":TelescopeGo<cr>", { desc = "Find Files (go)", noremap = true, silent = true })
-map("n", "<leader>fm", ":TelescopeMD<cr>", { desc = "Find Files (md)", noremap = true, silent = true })
-map("n", "<leader>fM", ":TelescopeMDX<cr>", { desc = "Find Files (mdx)", noremap = true, silent = true })
-map("n", "<leader>fj", ":TelescopeTSX<cr>", { desc = "Find Files (tsx)", noremap = true, silent = true })
-map("n", "<leader>fJ", ":TelescopeTS<cr>", { desc = "Find Files (ts)", noremap = true, silent = true })
-map("n", "<leader>fa", ":TelescopeAstro<cr>", { desc = "Find Files (astro)", noremap = true, silent = true })
-
--- Go to defenition in vertical and horizontal view
-map("n", "g|", "<cmd>vertical winc ]<cr>", { desc = "Go to vertical defenition", silent = true })
-map("n", "g-", "<cmd>horizontal winc ]<cr>", { desc = "Go to horizontal defenition", silent = true })
+----------------------------
+----- TELESCOPE/FILE'S -----
+----------------------------
 
 -- Oil
-map("n", "-", "<cmd>Oil<cr>", { desc = "Open Oil", silent = true })
+map("n", "-", "<cmd>Oil<CR>", { desc = "Open Oil", silent = true })
 
--- Center text in insert mode
-map("i", "<c-z>", "<esc>zz", { noremap = true, silent = true })
+-- Telescope pull-up notes
+map("n", "<leader>fn", ":SearchNotes<CR>", { desc = "Lookup notes", noremap = true, silent = true })
 
--- Text manipulation
--- Paste in insert mode and perseve formating
+-- Telescope lookup specific file type
+map("n", "<leader>fG", ":TelescopeGo<CR>", { desc = "Find Files (go)", noremap = true, silent = true })
+map("n", "<leader>fm", ":TelescopeMD<CR>", { desc = "Find Files (md)", noremap = true, silent = true })
+map("n", "<leader>fM", ":TelescopeMDX<CR>", { desc = "Find Files (mdx)", noremap = true, silent = true })
+map("n", "<leader>fj", ":TelescopeTSX<CR>", { desc = "Find Files (tsx)", noremap = true, silent = true })
+map("n", "<leader>fJ", ":TelescopeTS<CR>", { desc = "Find Files (ts)", noremap = true, silent = true })
+map("n", "<leader>fa", ":TelescopeAstro<CR>", { desc = "Find Files (astro)", noremap = true, silent = true })
+
+-- Go to defenition in vertical and horizontal view
+map("n", "g|", "<cmd>vertical winc ]<CR>", { desc = "Go to vertical defenition", silent = true })
+map("n", "g-", "<cmd>horizontal winc ]<CR>", { desc = "Go to horizontal defenition", silent = true })
+
+-- Save file to arrow
+-- to toggle arrow menu is the letter R
+map("n", "<leader>a", require("arrow.persist").toggle, { desc = "Arrow File Mappings (save)", silent = true })
+
+------------------------------------
+----- CURSOR/TEXT MANIPULATION -----
+------------------------------------
+
+-- <Delete> key functionality
+map("n", "<C-x>", "<Del>", { noremap = true, silent = true })
+map("i", "<C-x>", "<Del>", { noremap = true, silent = true })
+
+-- PASTE IN INSERT MODE AND PERSEVE FORMATING
 map(
   "i",
   "<C-o>:set paste<CR><C-r>+<C-o>:set nopaste<CR>",
@@ -41,40 +63,34 @@ map(
 )
 
 -- Change the first letter of word from lower to upper and vice versa
-map("i", "<a-d>", "<esc>b~ea", { noremap = true, silent = true })
-map("n", "<a-d>", "<esc>b~e", { noremap = true, silent = true })
+map("i", "<A-d>", "<esc>b~ea", { noremap = true, silent = true })
+map("n", "<A-d>", "<esc>b~e", { noremap = true, silent = true })
 
--- Cursor manipulation
 -- Jump to end of of the line while in insert mode
-map("i", "<a-a>", "<c-o>A", { noremap = true, silent = true })
+map("i", "<A-a>", "<c-o>A", { noremap = true, silent = true })
 
--- Resizing of windows
-map("n", "<a-.>", "<c-w>10<")
-map("n", "<a-,>", "<c-w>10>")
-map("n", "<a-t>", "<c-W>+10")
-map("n", "<a-s>", "<c-W>-10")
+-- Center text in insert mode
+map("i", "<C-z>", "<esc>zz", { noremap = true, silent = true })
+
+-------------------------
+----- MISC COMMANDS -----
+-------------------------
 
 -- Open url under cursor
-map("n", "gx", "<esc>:URLOpenUnderCursor<cr>", { silent = true })
+map("n", "gx", "<esc>:URLOpenUnderCursor<CR>", { silent = true })
 
--- MY SPECIAL COMMANDS --
--- <leader>o --
-
--- Telescope | search bookmarks
-map("n", "<leader>ob", "<cmd>BrowserBookmarks<cr>", { desc = "Search browser bookmarks", silent = true })
-
--- Gopls sometimes doesnt pick newly imported packages so manual restart is required
-map("n", "<leader>oL", "<cmd>LspRestart<cr>", { desc = "Restart gopls", silent = true })
-
--- ZenMode and Twilight
-map("n", "<leader>oz", "<cmd>ZenMode<cr>", { desc = "Zen Mode", silent = true })
-map("n", "<leader>ot", "<cmd>Twilight<cr>", { desc = "Twilight Mode", silent = true })
-
--- Save all files
-map("n", "<leader>os", "<cmd>wa<cr>", { desc = "Save All files", noremap = true, silent = true })
-
--- Save file to arrow
-map("n", "<leader>A", require("arrow.persist").toggle, { desc = "Arrow File Mappings (save)", silent = true })
+-- Search bookmarks
+map("n", "<leader>ob", "<cmd>BrowserBookmarks<CR>", { desc = "Search browser bookmarks", silent = true })
 
 -- Codeium turn off / on
-map("n", "<leader>oc", "<cmd>CodeiumToggle<cr>", { desc = "Codeium Toggle", silent = true })
+map("n", "<leader>oc", "<cmd>CodeiumToggle<CR>", { desc = "Codeium Toggle", silent = true })
+
+-- Gopls sometimes doesnt pick newly imported packages so manual restart is required
+map("n", "<leader>oL", "<cmd>LspRestart<CR>", { desc = "Restart gopls", silent = true })
+
+-- Save all files
+map("n", "<leader>os", "<cmd>wa<CR>", { desc = "Save All files", noremap = true, silent = true })
+
+-- ZenMode and Twilight
+map("n", "<leader>oz", "<cmd>ZenMode<CR>", { desc = "Zen Mode", silent = true })
+map("n", "<leader>ot", "<cmd>Twilight<CR>", { desc = "Twilight Mode", silent = true })
