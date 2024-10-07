87d465e458d5057dcff6ebeecd71b059f6f28aa1
miloshadzic86 <zvrkatwork@gmail.com>
2024-10-07 12:28:02 +0200

    Tidy up

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,4 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "ThePrimeagen/harpoon", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
-
-  -- these two are realated to Markdown
 }
