7110843ce891a75469463b6f7f25cb83f7c24656
miloshadzic86 <zvrkatwork@gmail.com>
2024-10-10 11:29:52 +0200

    added icons back

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -95,7 +95,7 @@ return {
             padding = { left = 1, right = 0 },
           },
           -- LazyVim.lualine.root_dir(),
-          -- { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { LazyVim.lualine.pretty_path(), separator = "" },
           -- stylua: ignore
           {
