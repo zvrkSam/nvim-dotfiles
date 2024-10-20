766c2d8d9f8e337c4efd00c29820df811669fa31
miloshadzic86 <zvrkatwork@gmail.com>
2024-10-20 07:23:03 +0200

    added snippets config file

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,8 @@
+return {
+  "L3MON4D3/LuaSnip",
+  config = function()
+    -- TODO:
+    -- require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/my_snippets" })
+    -- require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/my_snippets/" })
+  end,
+}
