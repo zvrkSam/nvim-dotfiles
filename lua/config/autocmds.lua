b0151a70a7f4356974055a8e4e83fa5657f493e0
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-03 11:28:59 +0100

    modifications

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,17 +2,25 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local usercmd = vim.api.nvim_create_user_command
+
+-- Prefer creating groups and assigning autocmds to groups, because it makes it easier to clear them
+augroup("mygroup", { clear = true })
+
+autocmd({ "FileType" }, {
   pattern = { "markdown", "json" },
   callback = function()
     vim.wo.conceallevel = 0
   end,
+  group = "mygroup",
 })
 
 --- Buffers ---
 
 local function delete_all_buffers(command_name)
-  vim.api.nvim_create_user_command(command_name, function()
+  usercmd(command_name, function()
     local bufs = vim.api.nvim_list_bufs()
     local current_buf = vim.api.nvim_get_current_buf()
 
@@ -29,7 +37,7 @@ end
 local telescope = require("telescope.builtin")
 
 local function search_by_file_type(command_name, file_pattern, prompt_title)
-  vim.api.nvim_create_user_command(command_name, function()
+  usercmd(command_name, function()
     telescope.find_files({
       find_command = { "rg", "--files", "--glob", file_pattern },
       prompt_title = prompt_title,
@@ -38,7 +46,7 @@ local function search_by_file_type(command_name, file_pattern, prompt_title)
 end
 
 local function search_by_directory(command_name, dir, prompt_title)
-  vim.api.nvim_create_user_command(command_name, function()
+  usercmd(command_name, function()
     telescope.find_files({
       cwd = vim.fn.expand(dir),
       prompt_title = prompt_title,
@@ -47,7 +55,7 @@ local function search_by_directory(command_name, dir, prompt_title)
 end
 
 local function grep_notes(command_name, dir, prompt_title)
-  vim.api.nvim_create_user_command(command_name, function()
+  usercmd(command_name, function()
     telescope.live_grep({
       cwd = vim.fn.expand(dir),
       type_filter = "md",
