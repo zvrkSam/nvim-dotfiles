119cf6e520812e3cbe681961a1996f394415cc5f
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-04 09:27:01 +0100

    changed open_mapping to alt-t

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -39,7 +39,7 @@ return {
         return vim.o.columns * 0.4
       end
     end,
-    open_mapping = [[<c-\>]],
+    open_mapping = [[<a-t>]],
     -- on_open = fun(t: Terminal), -- function to run when the terminal opens
     -- on_close = fun(t: Terminal), -- function to run when the terminal closes
     -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
