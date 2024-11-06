cb2873af0b384a96e2629b8efc1ced9318127116
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-06 01:51:49 +0100

    merged blink.cmp and nvim-cmp into one file

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,40 @@
+-- blink.cmp => https://github.com/Saghen/blink.cmp
+-- this plugin is awesome and fast but it is in BETA and it is lacking features
+-- this will be a good replacement for nvim-cmp
+
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+      }
+      -- Disables ghost text
+      opts.experimental = {
+        -- ghost_text = false,
+      }
+      return opts
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+
+    opts = {
+      windows = {
+        autocomplete = {
+          border = "rounded",
+        },
+        documentation = {
+          border = "rounded",
+          max_height = 50,
+          auto_show_delay_ms = 250,
+        },
+        signature_help = {
+          border = "rounded",
+        },
+      },
+    },
+  },
+}
