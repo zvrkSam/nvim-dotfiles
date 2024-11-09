f7a15ff8524d9683d32eca1d6bb05a29c27ef54a
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-09 15:48:00 +0100

    zen and twilight are now in ui.lua

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "Wansmer/treesj",
+    keys = {
+      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+    },
+    opts = { use_default_keymaps = false, max_join_length = 150 },
+  },
+  { "folke/zen-mode.nvim" },
+  { "folke/twilight.nvim" },
+}
