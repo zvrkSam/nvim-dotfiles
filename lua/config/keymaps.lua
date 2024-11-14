788ff3d81b2ec87ad294a57b10f2b060c7a8cda1
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-15 00:55:32 +0100

    removed toggle terms keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,16 +27,6 @@ map("n", "<A-s>", "<c-W>-10") -- vertical expand ( bottom )
 ----- TERMINAL -----
 --------------------
 
--- ToggleTerm
--- function _G.set_terminal_keymaps()
---   local opts = { noremap = true }
---   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
---   vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
---   vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
---   vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
---   vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
--- end
-
 ----------------------------
 ----- TELESCOPE/FILE'S -----
 ----------------------------
