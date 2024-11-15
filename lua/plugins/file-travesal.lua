23fa6e9902b66d0d27e2768c33a61db7b911bf4e
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-15 14:54:47 +0100

    remaped keymaps for opening splits in oil

--- lua/plugins/file-travesal.lua
+++ lua/plugins/file-travesal.lua
@@ -18,6 +18,8 @@ return {
         max_height = 65,
       },
       keymaps = {
+        ["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        ["<C-x>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
         ["<c-c>"] = false,
         ["q"] = "actions.close",
       },
