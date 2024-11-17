0a5b09dd19484f2c6c0cf134068d93ce25562b60
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-17 06:45:47 +0100

    added news for neovim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,7 +9,11 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = { news = { neovim = true } },
+    },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- { import = "lazyvim.plugins.extras.lang.docker" },
