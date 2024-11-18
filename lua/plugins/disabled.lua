e4e08bd24618ccf77dbf1bd81f6050a15a6c1119
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-18 18:29:34 +0100

    disabled dashboard-nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "ThePrimeagen/harpoon", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
 }
