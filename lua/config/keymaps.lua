55b033aa54153a060ac145ec79e12ed3bde8df56
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-20 12:37:11 +0100

    added floating terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,6 +27,31 @@ map("n", "<A-s>", "<c-W>-10") -- vertical expand ( bottom )
 ----- TERMINAL -----
 --------------------
 
+map({ "n", "t" }, "<A-F>", function()
+  local current_dir = vim.fn.expand("%:p:h")
+  if current_dir == "" or vim.fn.isdirectory(current_dir) == 0 then
+    current_dir = vim.fn.getcwd()
+  end
+  local in_terminal = vim.bo.buftype == "terminal"
+  if in_terminal then
+    vim.cmd("hide")
+  else
+    Snacks.terminal.toggle("zsh", {
+      cwd = current_dir,
+      env = {
+        TERM = "screen-256color",
+      },
+      win = {
+        style = "terminal",
+        relative = "editor",
+        width = 0.85,
+        height = 0.90,
+        border = "rounded",
+      },
+    })
+  end
+end, { desc = "Toggle floating terminal" })
+
 ----------------------------
 ----- TELESCOPE/FILE'S -----
 ----------------------------
