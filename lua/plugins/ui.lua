76b5133b465d1791a6d470ecd8af5a9e43370f45
miloshadzic86 <zvrkatwork@gmail.com>
2024-11-24 16:53:11 +0100

    disabled backdrop from flash

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -6,6 +6,11 @@ return {
     },
     opts = { use_default_keymaps = false, max_join_length = 150 },
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = { highlight = { backdrop = false } },
+  },
   { "folke/zen-mode.nvim" },
   { "folke/twilight.nvim" },
 }
