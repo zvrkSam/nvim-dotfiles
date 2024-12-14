f4978a42ef166eb2e0e7d0297a03779bc770f225
zvrkSam <zvrkatwork@gmail.com>
2024-12-14 04:11:00 +0100

    updated in favour of fzf-lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,35 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      -- there a bug with layout_strategy = "flex"
-      -- flip_columns are affected by it
-      -- layout_strategy = "flex",
-      layout_strategy = "vertical",
-      layout_config = {
-        -- flip_columns = 150,
-        vertical = {
-          preview_height = 0.6,
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        -- there a bug with layout_strategy = "flex"
+        -- flip_columns are affected by it
+        -- layout_strategy = "flex",
+        layout_strategy = "vertical",
+        layout_config = {
+          -- flip_columns = 150,
+          vertical = {
+            preview_height = 0.6,
+          },
+        },
+        file_ignore_patterns = {
+          "node_modules",
+          ".lock",
         },
       },
-      file_ignore_patterns = {
-        "node_modules",
-        ".lock",
+    },
+  },
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      winopts = {
+        preview = {
+          vertical = "up:65%",
+        },
+      },
+      files = {
+        fzf_opts = { ["--layout"] = "default" },
       },
     },
   },
