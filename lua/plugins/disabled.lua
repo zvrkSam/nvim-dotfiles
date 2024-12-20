f2d848b3ec71a407c34c66d7d3e9993a2ae2b59d
zvrkSam <zvrkatwork@gmail.com>
2024-12-20 12:42:20 +0100

    disabled catppuccin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "ThePrimeagen/harpoon", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 }
