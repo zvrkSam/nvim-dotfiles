83009268989e14c3a3ed2cd2385f58a113997284
zvrkSam <zvrkatwork@gmail.com>
2025-02-03 13:40:46 +0100

    removed telescope

--- lua/plugins/fzf-telescope.lua
+++ lua/plugins/fzf-telescope.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    enabled = true,
-    opts = {
-      defaults = {
-        layout_strategy = "vertical",
-        layout_config = {
-          vertical = {
-            preview_height = 0.6,
-          },
-        },
-        file_ignore_patterns = {
-          "node_modules",
-          ".lock",
-        },
-      },
-    },
-  },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   enabled = true,
+  --   opts = {
+  --     defaults = {
+  --       layout_strategy = "vertical",
+  --       layout_config = {
+  --         vertical = {
+  --           preview_height = 0.6,
+  --         },
+  --       },
+  --       file_ignore_patterns = {
+  --         "node_modules",
+  --         ".lock",
+  --       },
+  --     },
+  --   },
+  -- },
   -- {
   --   "ibhagwan/fzf-lua",
   --   opts = {
