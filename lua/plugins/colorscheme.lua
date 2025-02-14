f39196848cc10db4ca1237f13045a99edfb65dcb
zvrkSam <zvrkatwork@gmail.com>
2025-02-14 22:34:25 +0100

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -135,8 +135,36 @@ return {
     },
   },
   {
+    "dgox16/oldworld.nvim",
+    enabled = false,
+    opts = {
+      variant = "oled", -- default, oled, cooler
+      styles = { -- You can pass the style using the format: style = true
+        comments = { italic = false }, -- style for comments
+        keywords = { italic = false }, -- style for keywords
+        identifiers = { italic = false }, -- style for identifiers
+        functions = { italic = false }, -- style for functions
+        variables = { italic = false }, -- style for variables
+        booleans = { italic = false }, -- style for booleans
+      },
+    },
+  },
+  {
+    "thesimonho/kanagawa-paper.nvim",
+    opts = {
+      transparent = true,
+      dimInactive = false,
+      commentStyle = { italic = false },
+      keywordStyle = { italic = false },
+      functionStyle = { italic = false },
+      typeStyle = { italic = false },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "oldworld",
+      -- colorscheme = "kanagawa-paper",
       colorscheme = "tokyonight",
     },
   },
