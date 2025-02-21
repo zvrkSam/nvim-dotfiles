f68e09e68a2bed60095f0b9e3e5ebf352d800ae0
zvrkSam <zvrkatwork@gmail.com>
2025-02-21 11:06:33 +0100

    cleaned up some old code

--- lua/plugins/file-travesal.lua
+++ lua/plugins/file-travesal.lua
@@ -1,6 +1,4 @@
 return {
-
-  -- Oil
   {
     "stevearc/oil.nvim",
     event = "VeryLazy",
@@ -25,42 +23,10 @@ return {
       },
     },
   },
-
-  -- neo-tree
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    event = "VeryLazy",
-    enabled = false,
-    opts = {
-      window = {
-        -- "right" | "left" | "float" | "current"
-        position = "float",
-        -- width = 30,
-        mappings = {
-          ["w"] = "close_all_nodes",
-          ["W"] = "expand_all_nodes",
-        },
-      },
-      -- hijack_netrw_behavior = "open_default",
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = false,
-          hide_dotfiles = false,
-          hide_gitignore = false,
-          hide_by_name = {
-            "node_modules",
-            ".git",
-          },
-        },
-      },
-    },
-  },
-
-  -- arrow is like primeagens harpoon but with lualine support out of the box
-  -- TODO: Add timeout option for popup menu | like which-key
-  {
+    -- TODO: Add timeout option for popup menu | like which-key
     "otavioschwanck/arrow.nvim",
+    event = "VeryLazy",
     lazy = true,
     opts = {
       show_icons = true,
@@ -69,14 +35,10 @@ return {
       separate_by_branch = true,
     },
   },
-
-  -- undo-tree
   {
     "mbbill/undotree",
     lazy = false,
   },
-
-  -- scrollEOF
   {
     "Aasim-A/scrollEOF.nvim",
     event = { "CursorMoved", "WinScrolled" },
