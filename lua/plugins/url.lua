5779827bd9d8fc064263aa8d748d16fa459a2bb7
zvrkSam <zvrkatwork@gmail.com>
2025-03-25 18:07:51 +0100

    gx now opens URL anywhere on the line

--- lua/plugins/url.lua
+++ lua/plugins/url.lua
@@ -9,7 +9,30 @@ return {
   },
   {
     "chrishrb/gx.nvim",
-    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    keys = {
+      {
+        "gx",
+        function()
+          -- Save current position
+          local save_cursor = vim.api.nvim_win_get_cursor(0)
+
+          -- Search for http:// or https:// on the current line
+          local line = vim.api.nvim_get_current_line()
+          local url_start = line:find("https?://")
+
+          if url_start then
+            -- Move cursor to the start of the URL
+            vim.api.nvim_win_set_cursor(0, { save_cursor[1], url_start - 1 })
+            -- Fire Browse command
+            vim.cmd("Browse")
+          end
+
+          -- Restores cursor position
+          vim.api.nvim_win_set_cursor(0, save_cursor)
+        end,
+        mode = { "n", "x" },
+      },
+    },
     cmd = { "Browse" },
     init = function()
       vim.g.netrw_nogx = 1 -- disable netrw gx
