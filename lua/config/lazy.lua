e2d526b076c42247df29e23d926c670300911236
zvrkSam <zvrkatwork@gmail.com>
2025-03-26 09:24:19 +0100

    removed comments and old code

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,17 +15,8 @@ require("lazy").setup({
       opts = { news = { neovim = true } },
     },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    -- { import = "lazyvim.plugins.extras.lang.docker" },
-    -- { import = "lazyvim.plugins.extras.lang.go" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.lang.markdown" },
-    -- { import = "lazyvim.plugins.extras.lang.tailwind" },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.yaml" },
-    -- { import = "lazyvim.plugins.extras.lang.astro" },
-    -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
+    -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "plugins" },
   },
   defaults = {
