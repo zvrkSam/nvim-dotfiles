d1bf2004ac8011d50986fd160ab73f3d5f7701b9
zvrkSam <zvrkatwork@gmail.com>
2025-04-02 08:56:47 +0200

    moved comments around and added two links for future todo

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,7 @@
--- templ specific commands
--- local templ_format = function()
---   local bufnr = vim.api.nvim_get_current_buf()
---   local filename = vim.api.nvim_buf_get_name(bufnr)
---   local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+-- Tips on how to improve virtual_lines and inline diagnostics
+-- https://www.reddit.com/r/neovim/comments/1jpbc7s/disable_virtual_text_if_there_is_diagnostic_in/
+-- https://www.reddit.com/r/neovim/comments/1jo9oe9/i_set_up_my_config_to_use_virtual_lines_for/
 --
---   vim.fn.jobstart(cmd, {
---     on_exit = function()
---       -- Reload the buffer only if it's still the current buffer
---       if vim.api.nvim_get_current_buf() == bufnr then
---         vim.cmd("e!")
---       end
---     end,
---   })
--- end
---
--- vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = templ_format })
--- vim.filetype.add({ extension = { templ = "templ" } })
-
 -- Disabled due to constant layout shifting
 -- Virtual lines
 -- vim.diagnostic.config({ virtual_lines = true })
@@ -58,3 +43,22 @@ return {
     },
   },
 }
+
+-- templ specific commands
+-- local templ_format = function()
+--   local bufnr = vim.api.nvim_get_current_buf()
+--   local filename = vim.api.nvim_buf_get_name(bufnr)
+--   local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+--
+--   vim.fn.jobstart(cmd, {
+--     on_exit = function()
+--       -- Reload the buffer only if it's still the current buffer
+--       if vim.api.nvim_get_current_buf() == bufnr then
+--         vim.cmd("e!")
+--       end
+--     end,
+--   })
+-- end
+--
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = templ_format })
+-- vim.filetype.add({ extension = { templ = "templ" } })
