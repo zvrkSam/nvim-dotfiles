ae4e3016339c5c9d0f09f66854f61248b9e704a4
zvrkSam <zvrkatwork@gmail.com>
2025-04-07 20:06:10 +0200

    added dot directory lookup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -52,3 +52,6 @@ search_by_file_type("FindMDX", "*.mdx", "Find mdx files")
 -- Note lookup
 search_by_directory("SearchNotes", "~/dev/notes", "Search notes by name")
 grep_notes("GrepNotes", "~/dev/notes/", "Grep in notes")
+
+-- Dot files lookup
+search_by_directory("SearchDots", "~/dev/dots/", "Grep dot files")
