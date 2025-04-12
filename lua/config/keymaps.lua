bd69f93268e66c0f23ec8f0592678ff1444860ae
zvrkSam <zvrkatwork@gmail.com>
2025-04-12 16:54:16 +0200

    updated bold text keybind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -201,16 +201,89 @@ map("n", "<leader>nx", function()
   vim.api.nvim_buf_set_lines(0, row - 1, row, false, { new_line })
 end, { desc = "Toggle markdown task" })
 
--- Bold word under cursor or select few words and bold them (.md files only)
-vim.api.nvim_create_augroup("markdown_bold", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  group = "markdown_bold",
-  pattern = "markdown",
-  callback = function()
-    map("n", "<leader>nb", 'ciw**<C-r>"**<Esc>', { buffer = true })
-    map("v", "<leader>nb", 'c**<C-r>"**<Esc>', { buffer = true })
-  end,
-})
+-- Took this these two keybinds linkarzu
+--
+-- In visual mode, check if the selected text is already bold and show a message if it is
+-- If not, surround it with double asterisks for bold
+vim.keymap.set("v", "<leader>nb", function()
+  -- Get the selected text range
+  local start_row, start_col = unpack(vim.fn.getpos("'<"), 2, 3)
+  local end_row, end_col = unpack(vim.fn.getpos("'>"), 2, 3)
+  -- Get the selected lines
+  local lines = vim.api.nvim_buf_get_lines(0, start_row - 1, end_row, false)
+  local selected_text = table.concat(lines, "\n"):sub(start_col, #lines == 1 and end_col or -1)
+  if selected_text:match("^%*%*.*%*%*$") then
+    vim.notify("Text already bold", vim.log.levels.INFO)
+  else
+    vim.cmd("normal 2gsa*")
+  end
+end, { desc = "Bold text" })
+
+-- -- Multiline unbold attempt
+-- -- In normal mode, bold the current word under the cursor
+-- -- If already bold, it will unbold the word under the cursor
+-- -- If you're in a multiline bold, it will unbold it only if you're on the
+-- -- first line
+vim.keymap.set("n", "<leader>nb", function()
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local current_buffer = vim.api.nvim_get_current_buf()
+  local start_row = cursor_pos[1] - 1
+  local col = cursor_pos[2]
+  -- Get the current line
+  local line = vim.api.nvim_buf_get_lines(current_buffer, start_row, start_row + 1, false)[1]
+  -- Check if the cursor is on an asterisk
+  if line:sub(col + 1, col + 1):match("%*") then
+    vim.notify("Cursor is on an asterisk, run inside the bold text", vim.log.levels.WARN)
+    return
+  end
+  -- Search for '**' to the left of the cursor position
+  local left_text = line:sub(1, col)
+  local bold_start = left_text:reverse():find("%*%*")
+  if bold_start then
+    bold_start = col - bold_start
+  end
+  -- Search for '**' to the right of the cursor position and in following lines
+  local right_text = line:sub(col + 1)
+  local bold_end = right_text:find("%*%*")
+  local end_row = start_row
+  while not bold_end and end_row < vim.api.nvim_buf_line_count(current_buffer) - 1 do
+    end_row = end_row + 1
+    local next_line = vim.api.nvim_buf_get_lines(current_buffer, end_row, end_row + 1, false)[1]
+    if next_line == "" then
+      break
+    end
+    right_text = right_text .. "\n" .. next_line
+    bold_end = right_text:find("%*%*")
+  end
+  if bold_end then
+    bold_end = col + bold_end
+  end
+  -- Remove '**' markers if found, otherwise bold the word
+  if bold_start and bold_end then
+    -- Extract lines
+    local text_lines = vim.api.nvim_buf_get_lines(current_buffer, start_row, end_row + 1, false)
+    local text = table.concat(text_lines, "\n")
+    -- Calculate positions to correctly remove '**'
+    -- vim.notify("bold_start: " .. bold_start .. ", bold_end: " .. bold_end)
+    local new_text = text:sub(1, bold_start - 1) .. text:sub(bold_start + 2, bold_end - 1) .. text:sub(bold_end + 2)
+    local new_lines = vim.split(new_text, "\n")
+    -- Set new lines in buffer
+    vim.api.nvim_buf_set_lines(current_buffer, start_row, end_row + 1, false, new_lines)
+    -- vim.notify("Unbolded text", vim.log.levels.INFO)
+  else
+    -- Bold the word at the cursor position if no bold markers are found
+    local before = line:sub(1, col)
+    local after = line:sub(col + 1)
+    local inside_surround = before:match("%*%*[^%*]*$") and after:match("^[^%*]*%*%*")
+    if inside_surround then
+      vim.cmd("normal gsd*.")
+    else
+      vim.cmd("normal viw")
+      vim.cmd("normal 2gsa*")
+    end
+    vim.notify("Bolded current word", vim.log.levels.INFO)
+  end
+end, { desc = "Bold text" })
 
 ----------------------------------
 ----- MISC [o]GROUP COMMANDS -----
