2abcadd2cc85611e9a6023c5c360f91f98c23b9e
zvrkSam <zvrkatwork@gmail.com>
2025-05-01 23:16:49 +0200

    removed plugins that were not being used at all

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "ThePrimeagen/harpoon", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
   -- { "echasnovski/mini.pairs", enabled = false },
 }
