32ec5e15db7da53e23694581eed77606e4e896ba
zvrkSam <zvrkatwork@gmail.com>
2025-05-08 03:44:36 +0200

    workaround for new mason version

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+-- NOTE: This file only exists until folke updates lazyvim
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
